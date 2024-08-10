a36ceb8a97a47b68f099b7049b6c0191dd6faec7
yen0224 <yen0224@proton.me>
2024-08-10 09:40:56 +0800

    chore: settings page routing

---
+++ pages/settings/index.vue
@@ -0,0 +1,7 @@
+<template>
+    <div class="w-screen h-screen bg-gray-600">
+        <div class="flex justify-center items-center h-full">
+            <div class="text-white text-4xl">settings page</div>
+        </div>
+    </div>
+</template>
