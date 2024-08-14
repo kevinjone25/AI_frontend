075e5e1f808abcea5fcaa2a6559223ced7b929fc
yen0224 <yen0224@proton.me>
2024-08-14 23:20:21 +0800

    feat: create scroll to top components

--- app.vue
+++ app.vue
@@ -2,5 +2,6 @@
   <div>
     <Navbar />
     <NuxtPage />
+    <ScrollToTop />
   </div>
 </template>
---
+++ components/scrollToTop.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="fixed bottom-0 right-0 p-4">
+        <button @click="scrollToTop" class="bg-blue-500 text-white p-2 rounded-lg">
+            Scroll to top
+        </button>
+    </div>
+</template>
+
+<script setup>
+import { onMounted } from 'vue'
+function scrollToTop() {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    })
+}
+onMounted(() => {
+    window.addEventListener('scroll', () => {
+        const scrollButton = document.querySelector('.scroll-to-top')
+        if (window.scrollY > 100) {
+            scrollButton.classList.add('visible')
+        } else {
+            scrollButton.classList.remove('visible')
+        }
+    })
+})
+</script>
