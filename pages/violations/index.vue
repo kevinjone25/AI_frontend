a3b211bf65139985b10a52169fc83941dfe894de
yen0224 <yen0224@proton.me>
2024-08-16 12:38:38 +0800

    route: violation page will push

--- pages/violations/index.vue
+++ pages/violations/index.vue
@@ -1,17 +1,4 @@
-<template>
-    <div class="w-screen h-screen">
-        <div class="flex flex-col justify-center h-full mx-10 lg:mx-20 xl:mx-40">
-            <header>
-                <div class=" text-slate-200 text-6xl font-black">違規紀錄<br/> </div>
-                <div class="text-white text-4xl">This page list all info</div>
-            </header>
-            <main>
-
-            </main>
-        </div>
-    </div>
-</template>
-
-<script setup lang="ts">
-
+<script setup>
+    const router = useRouter()
+    router.push({ path: '/violations/all' })
 </script>
