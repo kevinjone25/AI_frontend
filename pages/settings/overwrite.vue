66e766e38e0ac7e15e4eaab55fde93f9c76eb862
yen0224 <yen0224@proton.me>
2024-08-21 02:08:31 +0800

    chore: overwrite page

---
+++ pages/settings/overwrite.vue
@@ -0,0 +1,7 @@
+<template>
+    <div>
+        <div class="flex justify-center items-center h-full">
+            <div class="text-white text-4xl">覆寫設置</div>
+            </div>
+    </div>
+</template>
