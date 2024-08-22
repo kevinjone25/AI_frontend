930e9ff0d3a3263d6c90faf0ad1ed98cf030c1ed
yen0224 <yen0224@proton.me>
2024-08-22 14:59:09 +0800

    feat: index connect to API

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,7 @@
 <template>
-    <div class="w-screen sm:h-screen flex justify-center items-center bg-gradient-to-tr from-gray-950 from-30% via-blue-950 via-95% to-sky-950 to-100% px-5 lg:px-20 xl:px-40 pt-16 text-white">
+    <div class="w-screen lg:h-screen flex justify-center items-center bg-gradient-to-tr from-gray-950 from-30% via-blue-950 via-95% to-sky-950 to-100% px-5 lg:px-20 xl:px-40 pt-16 text-white">
         <main class="w-full flex flex-wrap justify-center items-center  border-yellow-600">
-            <div class="left h-screen sm:h-full md:h-2/3 lg:w-1/2 w-full flex flex-col text-white sm:justify-between   border-blue-600">
+            <div class="left h-screen sm:h-2/3 lg:h-full lg:w-1/2 w-full flex flex-col text-white lg:justify-between   border-blue-600">
                 <div class="w-full mt-16 sm:mt-40 mb-20">
                     <h1 class="text-6xl m-1">系統總覽</h1>
                     <p class="text-2xl m-1">
@@ -9,7 +9,7 @@
                     </p>
                 </div>
 
-                <div class="flex flex-row flex-wrap justify-end">
+                <div class="flex flex-row flex-wrap justify-end lg:justify-start">
                     <nuxt-link to="/violations/criticals">
                         <div class="w-32 h-32 xl:w-44 xl:h-44 flex flex-col justify-between bg-red-500 bg-opacity-50 rounded-xl m-1 p-4 hover:translate-x-0.5 hover:bg-red-800 duration-300">
                         <h2 class="font-bold text-2xl xl:text-3xl text-red-50">嚴重</h2>
@@ -38,11 +38,11 @@
                 </div>
             </div>
 
-            <div class="right h-screen sm:h-full md:h-2/3 lg:w-1/2 w-full flex items-center text-slate-300   ">
+            <div class="right h-screen lg:h-full lg:w-1/2 w-full flex items-center text-slate-300   ">
                 <div class="w-full h-2/3 bg-slate-600 rounded-xl bg-opacity-50 p-4">
                     <div class="w-full h-1/6 text-md m-4">
                         <h2 class="text-4xl">最新紀錄</h2>
-                        <p>共 {{ events }} 筆資料, 最後更新於: YYYY/MM/DD HH:MM:SS</p>
+                        <p>共 {{ events }} 筆資料, 最後更新於: {{ last_update }}</p>
                     </div>
                     
                     <div class="w-full h-5/6">
@@ -63,6 +63,18 @@
 
 <script setup>
 
+    const config = useRuntimeConfig()
+    const api_url = config.public.backend_url+'violation-statistics'
+
+    //get today set to 2024-08-01 00:00:00
+    const today = new Date('2024-08-01')
+    today.setHours(0,0,0,0)
+    const tomorrow = new Date()
+    tomorrow.setDate(tomorrow.getDate()+1)
+    tomorrow.setHours(0,0,0,0)
+
+    const reqUrl = api_url + '?startTime=' + today.toISOString() + '&endTime=' + tomorrow.toISOString()
+
     const msg = ref('')
     const events = ref(0)
     const criticals = ref(0)
@@ -73,112 +85,74 @@
     const criticals_num = ref('0')
     const warns_num = ref('0')
     const events_num = ref('0')
-    // mock data
-    /*
-        level: 0 - normal, 1 - warning, 2 - critical
-    */
-    const res = ref([
-        {
-            "timestamp": "2024-08-08T07:34:17.320Z",
-            "cameraId": "01機房左側",
-            "violationType": "進食",
-            "ruleId": "201"
-        },
-        {
-            "timestamp": "2024-08-09T07:34:17.320Z",
-            "cameraId": "01機房左側",
-            "violationType": "私人設備拍攝行為",
-            "ruleId": "202"
-        },{
-            "timestamp": "2024-08-10T07:34:17.320Z",
-            "cameraId": "01機房左側",
-            "violationType": "機櫃門開啟",
-            "ruleId": "101"
-        },{
-            "timestamp": "2024-08-10T07:34:17.320Z",
-            "cameraId": "01機房左側",
-            "violationType": "人員進入機房",
-            "ruleId": "001"
-        },{
-            "timestamp": "2024-08-08T07:34:17.320Z",
-            "cameraId": "01機房左側",
-            "violationType": "進食",
-            "ruleId": "201"
-        },
-        {
-            "timestamp": "2024-08-09T07:34:17.320Z",
-            "cameraId": "01機房左側",
-            "violationType": "私人設備拍攝行為",
-            "ruleId": "202"
-        },{
-            "timestamp": "2024-08-10T07:34:17.320Z",
-            "cameraId": "01機房左側",
-            "violationType": "機櫃門開啟",
-            "ruleId": "101"
-        },{
-            "timestamp": "2024-08-10T07:34:17.320Z",
-            "cameraId": "01機房左側",
-            "violationType": "人員進入機房",
-            "ruleId": "001"
+    const last_update = ref('')
+    
+    // request objects
+    const res = ref([])
+    const loading = ref(true)
+    
+    // request function
+    async function fetchData(){
+        // send request, set CORS header
+        try{
+            const response = await fetch(reqUrl)
+            const data = await response.json()
+            res.value = data.violations
+        }catch(e){
+            console.log(e.message)
+            msg.value = '連線錯誤！'
+            loading.value = false
+            return
         }
-    ])
-
-    res.value.forEach((item) => {
-        if(item.ruleId > 200){
-            criticals.value++
-        }else if(item.ruleId > 100){
-            warns.value++
-        }
-        events.value++
-
-        //parse timestamp
-        item.timestamp = new Date(item.timestamp).toLocaleString()
-
-        //color
-        if(item.ruleId > 200){
-            item.color = 'bg-red-500'
-            item.level = '嚴重'
-        }
-        else if(item.ruleId > 100){
-            item.color = 'bg-yellow-500'
-            item.level = '警告'
-        }
-        else{
-            item.color = 'bg-gray-500'
-            item.level = '一般'
+        
+        // iterate res
+        for(let i=0; i<res.value.length; i++){
+            if(res.value[i].ruleId >= 200){
+                res.value[i].color = 'bg-red-500'
+                criticals.value++
+            }else if(res.value[i].ruleId >= 100){
+                res.value[i].color = 'bg-yellow-500'
+                warns.value++
+            }else{
+                res.value[i].color = 'bg-gray-500'
+            }
         }
 
-    })
+        events.value = res.value.length
+        // set message
+        events_num.value = res.value.length
 
-    // set message
-    events_num.value = events.value
-    if(events.value>99)
-        events_num.value = '99+' 
-
-    if(criticals.value>0 || warns.value>0) {
-        if(criticals.value){
-            criticals_num.value = criticals.value
-            msg.value = '目前有 ' + criticals.value + ' 筆嚴重違規紀錄,'
-            // avoid of overflow
-            if(criticals.value > 99)
-                criticals_num.value = '99+'
+        if(events.value>99)
+            events_num.value = '99+' 
+        if(criticals.value>0 || warns.value>0) {
+            if(criticals.value){
+                criticals_num.value = criticals.value
+                msg.value = '目前有 ' + criticals.value + ' 筆嚴重違規紀錄,'
+                // avoid of overflow
+                if(criticals.value > 99)
+                    criticals_num.value = '99+'
+            }
+            if(warns.value){
+                warns_num.value = warns.value
+                msg.value += '目前有 ' + warns.value + ' 筆警告紀錄,'
+                // avoid of overflow
+                if(warns.value > 99)
+                    warns_num.value = '99+'
+            }
+            msg.value += '請查看!'
+        }else if(criticals.value==0 && warns.value==0){
+            msg.value = '目前沒有新的違規紀錄！'
+        }else{
+            msg.value = '錯誤！'
+            criticals_num.value = 'Error'
+            warns_num.value = 'Error'
+            events_num.value = 'Error'
         }
-        if(warns.value){
-            warns_num.value = warns.value
-            msg.value += '目前有 ' + warns.value + ' 筆警告紀錄,'
-            // avoid of overflow
-            if(warns.value > 99)
-                warns_num.value = '99+'
-        }
-        msg.value += '請查看!'
-    }else if(criticals.value==0 && warns.value==0){
-        msg.value = '目前沒有新的違規紀錄！'
-    }else{
-        msg.value = '錯誤！'
-        criticals_num.value = 'Error'
-        warns_num.value = 'Error'
-        events_num.value = 'Error'
+    
+        loading.value = false
+        last_update.value = new Date().toLocaleString()
     }
+    
 
     function scrollToBottom(){
         window.scrollTo({
@@ -186,5 +160,15 @@
             behavior: 'smooth'
         })
     }
+
+    //fetch when onMounted
+    onMounted(async() => {
+        await fetchData()
+    })
+
+    onUpdated(async () => {
+        // update last update time
+        await fetchData()
+    })
 
 </script>
