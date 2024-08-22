5d5664c47c96c3a9fce07973338b2073c49c3007
yen0224 <yen0224@proton.me>
2024-08-22 19:42:10 +0800

    chore: rename navbar

--- components/navbar.vue
+++ components/navbar.vue
@@ -2,7 +2,7 @@
     <div class="flex text-white h-16 min-w-full fixed bg-neutral-900 bg-opacity-10 border-gray-800 border-b justify-center items-center backdrop-blur-lg z-50">
         <div class="flex w-3/4 px-1 justify-end gap-5 font-sans font-semibold">
             <div class="col">
-                <nuxt-link to="/" class="">主控板</nuxt-link>
+                <nuxt-link to="/" class="">總覽</nuxt-link>
             </div>
             <div class="col">
                 <nuxt-link to="/violations" class="">紀錄</nuxt-link>
